3bda11deba1e6c3a49eb2879c378c7bddd0fba22
kinyah <kinyanjuisamwel07@gmail.com>
2023-02-28 04:52:57 +0300

    production settings redone

--- rashel_agencies/settings.py
+++ rashel_agencies/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gm)fnxvlmlja*+!@6#429)j4k=vgtgn@)!*abx*zy^-m1ry%x)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
